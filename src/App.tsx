ef098a164b7d9f64ef7e69b9c85ba4ba281e41ac
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-22 10:43:03 +0200

    Add information about setting github actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ function App() {
   return (
     <div>
       <h1>Github actions practice</h1>
+      <h2>First your actions a name</h2>
+      <h2>Then you need to give it a run-name</h2>
+      <h3>Then set it to listen an event. This could be for example:</h3>
+      <p>on:</p>
+      <ul>
+        <li>push</li>
+        <li>pull_request</li>
+        <li>fork</li>
+      </ul>
     </div>
   )
 }
