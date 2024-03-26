7334c8eee318ed944b6c616e027c2d199176b643
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-26 13:12:09 +0200

    Add linting action

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest:true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '.github'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
